6f058515ad3e6eeb1ff5acaf1fc97d30a426d232
Sakilluhar <sakilluharpc@gmail.com>
2023-11-21 16:43:04 +0530

    cyber-sale changes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 'use client'
+import { useRouter } from 'next/router.js'
 import Home from '../src/Components/Home/Home.jsx'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 
-const index = () => {
+const Index = () => {
+  const router = useRouter()
+  // client side rendering route get
+  useEffect(() => {
+    if (router.pathname) {
+      router.replace(window.location.pathname + window.location.search)
+    }
+}, [])
   return (
     <>
 
@@ -12,4 +20,4 @@ const index = () => {
   )
 }
 
-export default index
+export default Index
